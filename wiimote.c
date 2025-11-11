d81578f998b469b290bfd365f1427147dea2533d
aaronloo1 <aaron.loo888@gmail.com>
2025-11-11 17:58:45 -0600

    Update wiimote.c

--- wiimote.c
+++ wiimote.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
+// Gyro-tester branch
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
